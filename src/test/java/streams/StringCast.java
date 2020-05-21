172f489ebfaf2a2a45d1d050073bb1e590bb2b4d
ricardomaximino <ricardomaximino@hotmail.com>
2020-05-21 14:00:16 +0200

    Cast String to integer

---
+++ src/test/java/streams/StringCast.java
@@ -0,0 +1,28 @@
+package streams;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class StringCast {
+
+    @Test
+    public void convert_string_to_integer(){
+        String strDate = "01/05/2020";
+        String[] strArray = strDate.split("/");
+        System.out.println(strArray[0] + " - " + strArray[1] + " - " + strArray[2]);
+
+        int day = Integer.parseInt(strArray[0]);
+        int month = Integer.parseInt(strArray[1]);
+        int year = Integer.parseInt(strArray[2]);
+        System.out.println("day:" + day + " month: " + month + " year: " + year);
+
+        LocalDate date = LocalDate.of(year,month,day);
+        System.out.println(date);
+
+        Date oldDate = Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+    }
+}
